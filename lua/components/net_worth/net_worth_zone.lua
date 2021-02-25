d2afde9930ae71dcb697a491635cfa3db8bffabe
Joe Gores <goresj@gmail.com>
2021-02-25 15:42:41 -0800

    Fix annoying errors when returning to main menu.

--- lua/components/net_worth/net_worth_zone.lua
+++ lua/components/net_worth/net_worth_zone.lua
@@ -11,7 +11,7 @@ function onObjectEnterScriptingZone(zone)
 end
 
 function onObjectLeaveScriptingZone(zone)
-  if (zone == self) then
+  if (zone and zone == self) then
     updatePlayerDisplay()
   end
 end
