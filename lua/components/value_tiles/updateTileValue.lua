ccdf1f10a5d26c07fedc870a526af61ae484c30c
Joe Gores <goresj@gmail.com>
2021-03-01 17:29:11 -0800

    WIP

--- lua/components/value_tiles/updateTileValue.lua
+++ lua/components/value_tiles/updateTileValue.lua
@@ -1,12 +1,16 @@
-function updateTileValue(object)
-  local value = object.getVar('val') or object.value
-  local quantity = object.getQuantity()
+-- ============================================================================
+-- This gets imported into Global because it's too expensive to put on every
+-- valueTile.
+-- ============================================================================
+function updateTileValue(obj)
+  local value = obj.getVar('val') or obj.value
+  local quantity = obj.getQuantity()
 
   if quantity > -1 then
     value = value * quantity
   end
 
-  object.value = value
+  obj.value = value
 end
 
 function onObjectEnterContainer(container, obj)
--- lua/components/value_tiles/valueTile.lua
+++ lua/components/value_tiles/valueTile.lua
@@ -13,12 +13,13 @@ function onSpawn()
   setTileValue()
 end
 
-function setTileValue(val)
-  local val = stringToNum(self.getGMNotes())
-  self.value = val
+function setTileValue()
+  self.value = stringToNum(self.getGMNotes())
+  self.setVar('val', self.value)
   if (self.hasTag('credits')) then
     self.value_flags = groups['credits']
   end
-  self.setVar('val', val)
+
+  -- Todo: Why the hell is this even necessary?!
   Global.call('updateTileValue', self)
 end
