764aefff3669c2c62482df388747482fa58fa63a
Joe Gores <goresj@gmail.com>
2021-03-08 16:57:55 -0800

    Add descriptions to cards.

---
+++ data/data.lua
@@ -0,0 +1,646 @@
+return {
+  cardboard = { {
+    type = "angled_square",
+    dimensions = {
+      width = 1.25,
+      height = 1.25
+    }
+  }, {
+    type = "board",
+    dimensions = {
+      width = 33,
+      height = 22
+    }
+  }, {
+    type = "culture_card",
+    dimensions = {
+      width = 1.5,
+      height = 2.5
+    }
+  }, {
+    type = "culture_token",
+    dimensions = {
+      width = 1.875,
+      height = 1.125
+    }
+  }, {
+    type = "circle_small",
+    dimensions = {
+      width = 0.75,
+      height = 0.75
+    }
+  }, {
+    type = "player_mat",
+    dimensions = {
+      width = 6.75,
+      height = 4.5
+    }
+  }, {
+    type = "player_token",
+    dimensions = {
+      width = 0.75,
+      height = 0.8
+    }
+  }, {
+    type = "rounded_rect",
+    dimensions = {
+      width = 1.125,
+      height = 0.75
+    }
+  } },
+  cultures = { {
+    id = 1,
+    name = "nillis",
+    iou = 80,
+    science = 1,
+    description = "These tranquil beings are masters of long-term cross-pollination. Young Nillis sprouts are vitually mindless, but as they grow, they become highly intelligent thinkers. Ancient Nillis can spend years immobile, meditating on the mysteries of life."
+  }, {
+    id = 2,
+    name = "volois",
+    iou = 120,
+    science = 4,
+    description = "These slug-like creatures comprise a collective intelligence that tends to be friendly yet enigmatic. The laws of time and space appear to bend wildly in their presence and it's unclear if they actually manipulate reality or if it's just a projected illusion."
+  }, {
+    id = 3,
+    name = "graw",
+    iou = 90,
+    science = 5,
+    description = "These degenerate, fish-like creatures are all that remain of the once proud race that ruled over the entire galaxy. Once highly advanced, they are now just primitive, superstitious brutes who worship their former students, the Qossuth."
+  }, {
+    id = 4,
+    name = "niks",
+    iou = 80,
+    science = 1,
+    description = "Small in stature and gregarious in demeanor, the Niks are impish creatures who chatter endlessly on all manner of topics. While they prefer living in rustic tree villages, they are by no means simple and are uniquely attuned to the natural world."
+  }, {
+    id = 5,
+    name = "dell",
+    iou = 60,
+    science = 2,
+    description = "The Dell are identified by their multiple eyes and tentacular mandibles. The Rastur invasion brought financial ruin to the Dell and they regressed to an industrial state. They seek to acquire valuables at little or no expense to themselves."
+  }, {
+    id = 6,
+    name = "humans",
+    iou = 90,
+    science = 3,
+    description = "Humans are unusually flexible beings, adapting to any environment they occupy with an uncanny knack for inventing and using tools. While not particularly intelligent, Humans can be potentially hazardous if left unsupervised too long."
+  }, {
+    id = 7,
+    name = "shenna",
+    iou = 90,
+    science = 5,
+    description = "These aggressive beings can be identified by the tail-like protuberances at the backs of their heads. They live and hunt in small clans and make excellent soldiers. Without external enemies to engage them, they tend to fight amongst themselves."
+  }, {
+    id = 8,
+    name = "yxklyx",
+    iou = 100,
+    science = 3,
+    description = "These asocial, crab-like creatures are brilliant biochemists, capable of fusing machinery and organic materials in revolutionary ways. They are often avoided, due in no small part to their tendency to view other races as potential test subjects."
+  }, {
+    id = 9,
+    name = "zum",
+    iou = 100,
+    science = 2,
+    description = "Zum society is peculiar in that it has no formal structures of government or commerce. Each of these large hive insects acts in perfect harmony with its brethren, all seemingly experiencing the exact same thoughts, preferences, and impulses."
+  }, {
+    id = 10,
+    name = "eeepeeep",
+    iou = 100,
+    science = 3,
+    description = "The origin of the Eeepeeep is a mystery. One explanation is that they are descendents of a single orphaned kitchen appliance from a long-lost galaxy. They are known for their friendly, logical natures and incredible insight into advanced technology."
+  }, {
+    id = 11,
+    name = "whynoms",
+    iou = 80,
+    science = 2,
+    description = "These pracical creatures are renowned for their dry wit. They are rumored to be descendents of irate, uncomfortable horses on a desperate quest to find a comfortable way to sit down. Their hoof-crafted goods are treasured throughout the galaxy."
+  }, {
+    id = 12,
+    name = "cholos",
+    iou = 90,
+    science = 1,
+    description = "These microscopic bacteria are ingenious bioengineers who excel at cellular manipulation. They can be difficult to communicate with given that they 'speak' by inflicting precise symptoms upon their host, making it sneeze in a specific rhythm."
+  }, {
+    id = 13,
+    name = "wollow",
+    iou = 90,
+    science = 5,
+    description = "These imposing giant lizards follow a philosophy of peace and love. They seek to spread their harmonious message throughout the galaxy. Given their terrifying stature, most beings are inclined to agree with whatever the Wollow are saying."
+  }, {
+    id = 14,
+    name = "qossuth",
+    iou = 120,
+    science = 4,
+    description = "Upon discovering the nature of the universe, the Qossuth evolved into amorphous masses of fog and lightning. Their behavior grew erratic, sometimes despondent, sometimes cruelly ambitious. Still, their profoundly disturbing art fetches a high price."
+  } },
+  demands = { {
+    name = "bionic perfume",
+    to = 4,
+    value = 60,
+    count = 2
+  }, {
+    name = "bionic perfume",
+    to = 5,
+    value = 60,
+    count = 1
+  }, {
+    name = "canned traits",
+    to = 4,
+    value = 60,
+    count = 1
+  }, {
+    name = "chicle liquor",
+    to = 11,
+    value = 60,
+    count = 1
+  }, {
+    name = "chicle liquor",
+    to = 12,
+    value = 60,
+    count = 2
+  }, {
+    name = "designer genes",
+    to = 2,
+    value = 40,
+    count = 2
+  }, {
+    name = "designer genes",
+    to = 14,
+    value = 60,
+    count = 2
+  }, {
+    name = "finest dust",
+    to = 6,
+    value = 50,
+    count = 2
+  }, {
+    name = "finest dust",
+    to = 7,
+    value = 50,
+    count = 2
+  }, {
+    name = "finest dust",
+    to = 9,
+    value = 50,
+    count = 3
+  }, {
+    name = "glorious junk",
+    to = 4,
+    value = 60,
+    count = 2
+  }, {
+    name = "glorious junk",
+    to = 6,
+    value = 40,
+    count = 4
+  }, {
+    name = "immortal grease",
+    to = 10,
+    value = 50,
+    count = 3
+  }, {
+    name = "immortal grease",
+    to = 12,
+    value = 50,
+    count = 2
+  }, {
+    name = "impossible furniture",
+    to = 13,
+    value = 70,
+    count = 1
+  }, {
+    name = "psychotic sculpture",
+    to = 3,
+    value = 90,
+    count = 1
+  }, {
+    name = "living toys",
+    to = 5,
+    value = 60,
+    count = 2
+  }, {
+    name = "living toys",
+    to = 7,
+    value = 40,
+    count = 3
+  }, {
+    name = "melf pelts",
+    to = 8,
+    value = 50,
+    count = 1
+  } },
+  encounters = {
+    hazards = {
+      blue = { 10, 10, 20, 20, 30, 30, 40 },
+      red = { 10, 20, 20, 30, 30, 40 },
+      yellow = { 10, 20, 30, 40 }
+    },
+    relics = { {
+      name = "Air Foil",
+      value = 80,
+      description = "Land and take off for 1 movement cost (instead of 2). Travel between surface cities for 1 movement cost."
+    }, {
+      name = "Auto Pilot",
+      value = 80,
+      description = "Set one die to 4 before rolling for speed."
+    }, {
+      name = "Gate Lock",
+      value = 100,
+      description = "Treat any telegate as a blue space."
+    }, {
+      name = "Jump Start",
+      value = 120,
+      description = "House rule: When choosing a navigation die, optionally jump to the same-numbered telegate before moving. (See instructions for original rule.)"
+    }, {
+      name = "Mulligan Gear",
+      value = 120,
+      description = "House rule: Optionally reroll one die after rolling for speed. (See instructions for original rule.)"
+    }, {
+      name = "Shield",
+      value = 60,
+      description = "Identical to purchasable shield equipment, but occupies no space on a ship."
+    }, {
+      name = "Spy Eye",
+      value = 100,
+      description = "House rule: May sneak a peak at an encounter token before choosing to ignore it by leaving it face down, or accept it by turning it face up. May observe a culture from anywhere within its system, rather than just in orbit. (See instructions for original rule.)"
+    }, {
+      name = "Switch Switch",
+      value = 100,
+      description = "Optionally switch navigation die with another speed die as often as desired anytime during a turn."
+    }, {
+      name = "Yellow Drive",
+      value = 80,
+      description = "Identical to purchasable yellow drive equipment, but occupies no space on a ship."
+    } },
+    spaceports = 3,
+    telegates = { 1, 2, 3, 4, 5, 6 }
+  },
+  equipment = { {
+    id = 1,
+    name = "shield"
+  }, {
+    id = 2,
+    name = "yellow drive"
+  }, {
+    id = 3,
+    name = "red drive"
+  }, {
+    id = 4,
+    name = "combo drive"
+  } },
+  goods = { {
+    name = "bionic perfume",
+    buy = 80,
+    sell = 140,
+    from = 1,
+    to = { 2, 3, 4, 5 },
+    count = 5,
+    factory = {
+      deed = 100,
+      buy = 60,
+      sell = 140
+    }
+  }, {
+    name = "canned traits",
+    buy = 140,
+    sell = 220,
+    from = 2,
+    to = { 3, 4, 5, 6 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 120,
+      sell = 240
+    }
+  }, {
+    name = "glorious junk",
+    buy = 30,
+    sell = 80,
+    from = 3,
+    to = { 4, 5, 6, 7 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 100,
+      sell = 200
+    }
+  }, {
+    name = "living toys",
+    buy = 20,
+    sell = 60,
+    from = 4,
+    to = { 5, 6, 7, 8 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 80,
+      sell = 180
+    }
+  }, {
+    name = "finest dust",
+    buy = 10,
+    sell = 50,
+    from = 5,
+    to = { 6, 7, 8, 9 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 120,
+      sell = 200
+    }
+  }, {
+    name = "rock videos",
+    buy = 120,
+    sell = 200,
+    from = 6,
+    to = { 7, 8, 9, 10 },
+    count = 5,
+    factory = {
+      deed = 100,
+      buy = 60,
+      sell = 160
+    }
+  }, {
+    name = "melf pelts",
+    buy = 50,
+    sell = 110,
+    from = 7,
+    to = { 8, 9, 10, 11 },
+    count = 6,
+    factory = {
+      deed = 100,
+      buy = 80,
+      sell = 150
+    }
+  }, {
+    name = "immortal grease",
+    buy = 50,
+    sell = 100,
+    from = 8,
+    to = { 9, 10, 11, 12 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 100,
+      sell = 200
+    }
+  }, {
+    name = "chicle liquor",
+    buy = 40,
+    sell = 90,
+    from = 9,
+    to = { 10, 11, 12, 13 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 140,
+      sell = 220
+    }
+  }, {
+    name = "pedigree bolts",
+    buy = 200,
+    sell = 300,
+    from = 10,
+    to = { 11, 12, 13, 14 },
+    count = 4,
+    factory = {
+      deed = 100,
+      buy = 100,
+      sell = 200
+    }
+  }, {
+    name = "impossible furniture",
+    buy = 110,
+    sell = 180,
+    from = 11,
+    to = { 12, 13, 14, 1 },
+    count = 5,
+    factory = {
+      deed = 100,
+      buy = 80,
+      sell = 160
+    }
+  }, {
+    name = "designer genes",
+    buy = 60,
+    sell = 120,
+    from = 12,
+    to = { 13, 14, 1, 2 },
+    count = 6,
+    factory = {
+      deed = 200,
+      buy = 160,
+      sell = 240
+    }
+  }, {
+    name = "megalith paperweight",
+    buy = 90,
+    sell = 160,
+    from = 13,
+    to = { 14, 1, 2, 3 },
+    count = 5,
+    factory = {
+      deed = 100,
+      buy = 80,
+      sell = 160
+    }
+  }, {
+    name = "psychotic sculpture",
+    buy = 160,
+    sell = 250,
+    from = 14,
+    to = { 1, 2, 3, 4 },
+    count = 4,
+    factory = {
+      deed = 200,
+      buy = 120,
+      sell = 250
+    }
+  } },
+  passengers = { {
+    name = "arms dealer",
+    from = 3,
+    to = 14,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "census taker",
+    from = 0,
+    to = 13,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "optometrist",
+    from = 5,
+    to = 0,
+    value = 180,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "philosopher",
+    from = 0,
+    to = 3,
+    value = 150,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "salesman",
+    from = 9,
+    to = 2,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "scientist",
+    from = 8,
+    to = 6,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "specimen",
+    from = 0,
+    to = 12,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "spy",
+    from = 7,
+    to = 0,
+    value = 150,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "tax collector",
+    from = 0,
+    to = 5,
+    value = 150,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "toaster",
+    from = 10,
+    to = 0,
+    value = 120,
+    cargo = 1,
+    ruleset = "classic"
+  }, {
+    name = "ambassador",
+    from = 13,
+    to = 4,
+    value = 120,
+    cargo = 1,
+    ruleset = "standard"
+  }, {
+    name = "caterer",
+    from = 0,
+    to = 1,
+    value = 300,
+    cargo = 2,
+    ruleset = "standard"
+  }, {
+    name = "diva",
+    from = 6,
+    to = 0,
+    value = 240,
+    cargo = 2,
+    ruleset = "standard"
+  }, {
+    name = "invader",
+    from = 7,
+    to = 11,
+    value = 180,
+    cargo = 1,
+    ruleset = "standard"
+  }, {
+    name = "politician",
+    from = 4,
+    to = 0,
+    value = 150,
+    cargo = 1,
+    ruleset = "standard"
+  } },
+  players = { {
+    color = "red",
+    home = {
+      culture = 10,
+      factory = 80,
+      spaceport = 160
+    }
+  }, {
+    color = "orange",
+    home = {
+      culture = 14,
+      factory = 160,
+      spaceport = 160
+    }
+  }, {
+    color = "yellow",
+    home = {
+      culture = 6,
+      factory = 80,
+      spaceport = 160
+    }
+  }, {
+    color = "green",
+    home = {
+      culture = 11,
+      factory = 80,
+      spaceport = 160
+    }
+  } },
+  sciences = { {
+    id = 1,
+    name = "biotech",
+    equipment = { 1 },
+    ships = { }
+  }, {
+    id = 2,
+    name = "industry",
+    equipment = { 2 },
+    ships = { 3, 4 }
+  }, {
+    id = 3,
+    name = "technology",
+    equipment = { 3 },
+    ships = { 1, 2 }
+  }, {
+    id = 4,
+    name = "metaphysics",
+    equipment = { 4 },
+    ships = { }
+  }, {
+    id = 5,
+    name = "primitive",
+    equipment = { },
+    ships = { }
+  } },
+  ships = { {
+    id = 1,
+    name = "scout",
+    cargo = 2,
+    speed = 3,
+    cost = 60
+  }, {
+    id = 2,
+    name = "clipper",
+    cargo = 2,
+    speed = 3,
+    cost = 120
+  }, {
+    id = 3,
+    name = "transport",
+    cargo = 3,
+    speed = 3,
+    cost = 240
+  }, {
+    id = 4,
+    name = "freighter",
+    cargo = 5,
+    speed = 2,
+    cost = 300
+  } }
+}
