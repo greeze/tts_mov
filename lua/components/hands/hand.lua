15da72d7f7049cdc9aaa9a8679e28eca94cb3f60
Joe Gores <goresj@gmail.com>
2021-07-17 19:24:31 -0700

    Flip goods to sell side when entering hand

--- lua/components/hands/hand.lua
+++ lua/components/hands/hand.lua
@@ -5,9 +5,22 @@ local function isCash(obj)
   return obj.hasTag('credits')
 end
 
+---@param obj table
+---@return boolean
+local function isGood(obj)
+  if (obj == nil) then return false end
+  return obj.hasTag('good')
+end
+
 ---@param zone table
 function onObjectEnterZone(zone, obj)
-  if (zone == self and isCash(obj)) then
-    obj.setRotation({0, 90, 0})
+  if (zone == self) then
+    if (isCash(obj)) then
+      obj.setRotation({0, 90, 0})
+    elseif (isGood(obj)) then
+      if (not obj.is_face_down) then
+        obj.flip()
+      end
+    end
   end
 end
